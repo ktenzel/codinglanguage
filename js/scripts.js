7b097ce19552f9966f6eed7d95ecb5188c63d106
Kyle Tenzeldam <ktenzel@gmail.com>
2017-12-01 09:27:54 -0800

    update jumbotron bg image and start questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  
+  $("form#quiz").submit(function(){
+    event.preventDefault();
+    var activityLevel = $("input:radio[name=activityLevel]:checked").val();
+
+    if (activityLevel === "notActive") {
+      result = add()
+    } else if (activityLevel === "mildlyActive") {
+      result = add
+    } else if (activityLevel === "overlyActive") {
+      result = add()
+    } else {
+      console.log("else");
+    }
+  })
 })
