82baa3f40299f1d49057054a122a32ebbf999d88
Kyle Tenzeldam <ktenzel@gmail.com>
2017-12-01 12:55:37 -0800

    update functions in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 $(document).ready(function(){
+//backend logic
+
+  function java(input1, input2, input3, input4, input5){
+    if ((input1 + input2 + input3 + input4 + input5) > 18) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function ruby(input1, input2, input3, input4, input5){
+    if ((input1 + input2 + input3 + input4 + input5) <= 18) && (input1 + input2 + input3 + input4 + input5) >= 10) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function react(input1, input2, input3, input4, input5){
+    if ((input1 + input2 + input3 + input4 + input5) < 9) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   $("form#whatLanguage").submit(function(){
     event.preventDefault();
 
-    if (activityLevel === "notActive") {
-      result = add()
-    } else if (activityLevel === "mildlyActive") {
-      result = add
-    } else if (activityLevel === "overlyActive") {
-      result = add()
-    } else {
+
+    else if java()
+   else {
       console.log("else");
     }
   })
