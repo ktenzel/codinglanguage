ee9c5c96f42075373ba1214b64d51794673d40b2
Kyle Tenzeldam <ktenzel@gmail.com>
2017-12-01 13:29:27 -0800

    add debugger to watch logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 //backend logic
-
   function java(input1, input2, input3, input4, input5){
     if ((input1 + input2 + input3 + input4 + input5) > 18) {
       return true;
@@ -10,7 +9,7 @@ $(document).ready(function(){
   }
 
   function ruby(input1, input2, input3, input4, input5){
-    if ((input1 + input2 + input3 + input4 + input5) <= 18) && (input1 + input2 + input3 + input4 + input5) >= 10) {
+    if ((input1 + input2 + input3 + input4 + input5) <= 18 && (input1 + input2 + input3 + input4 + input5) >= 10) {
       return true;
     } else {
       return false;
@@ -25,12 +24,47 @@ $(document).ready(function(){
     }
   }
   $("form#whatLanguage").submit(function(){
+
+    var frontEnd = parseInt($("#frontEnd").val());
+    var backEnd = parseInt($("#backEnd").val());
+    var large = parseInt($("#large").val());
+    var small = parseInt($("#small").val());
+    var windowsYes = parseInt($("#windowsYes").val());
+    var windowsNo = parseInt($("#windowsNo").val());
+    var databaseYes = parseInt($("#databaseYes").val());
+    var databaseNo = parseInt($("#databaseNo").val());
+    var platDesktop = parseInt($("#platDesktop").val());
+    var platWeb = parseInt($("#platWeb").val());
+    var platMobile = parseInt($("#platMobile").val());
+
+    var FrontendBackend = parseInt($("#FrontendBackend").val());
+    var CompanySize = parseInt($("#CompanySize").val());
+    var windows = parseInt($("#windows").val());
+    var preference = parseInt($("#preference").val());
+    var platform = parseInt($("#platform").val());
+
+
+
+
     event.preventDefault();
-
-
-    else if java()
-   else {
+    debugger;
+    if (java(FrontendBackend, CompanySize, windows, preference, platform)){
+      $("#java").show();
+      $("#react").hide();
+      $("#ruby").hide();
+    } else if (ruby(FrontendBackend, CompanySize, windows, preference, platform)){
+      $("#java").hide();
+      $("#react").hide();
+      $("#ruby").show();
+    } else if (react(FrontendBackend, CompanySize, windows, preference, platform)){
+      $("#java").hide();
+      $("#react").show();
+      $("#ruby").hide();
+    } else {
       console.log("else");
     }
-  })
-})
+  });
+});
+
+// frontEnd, backEnd, large, small, windowsYes, windowsNo, databaseYes, databaseNo, platDesktop, platWeb, platMobile
+// FrontendBackend, CompanySize, windows, preference, platform
